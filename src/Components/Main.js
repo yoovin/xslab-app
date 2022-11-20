ddc631b332372532ff78a3f2ed5c8f532f42c894
yoovin <soo22839@gmail.com>
2022-11-20 15:17:20 +0900

    폰트 및 아이콘추가

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import Navi from './Navi'
+
+import styles from './Styles'
+
+const infos = [
+    {
+        infoTitle:"부팅",
+        infoIcon:[],
+        infoNum: [],
+        infoText: [],
+    }
+]
 
 const Main = () => {
+
+    const title = <TouchableOpacity>
+        <Image source={require("../../assets/image/title.png")} style={{width: 130, height: 17, resizeMode: 'contain'}}/>
+    </TouchableOpacity>
+
   return (
-    <View>
-      <Text>Main</Text>
+    <View style={{flex:1}}>
+        <Navi title={title}/>
+      <View style={styles.infoContainer}>
+            <View style={styles.infoView}>
+                <Text>부팅</Text>
+            </View>
+            <View style={styles.infoView}></View>
+            <View style={styles.infoView}></View>
+            <View style={styles.infoView}></View>
+      </View>
+      <View style={{flex:5, backgroundColor: 'aqua'}}>
+            
+      </View>
+      <View style={{flex:1, backgroundColor: 'pink'}}>
+            
+      </View>
+
     </View>
   )
 }
--- src/Components/Styles.js
+++ src/Components/Styles.js
@@ -4,6 +4,46 @@ import {RFPercentage} from "react-native-responsive-fontsize"
 // const mainColor = ''
 
 export default StyleSheet.create({
+    /*
+    ===== NAVI COMPONENT =====
+    */
+
+    viewContainer: {
+        flex:1.5
+    },
+    contentContainer: {
+        flex: 1,
+        flexDirection:'row'
+    },
+    leftView:{
+        flex:1
+    },
+    titleView: {
+        flex: 4.5,
+        alignItems:'center',
+    },
+    titleText: {
+        // fontFamily: 'BMJUA',
+        fontSize: RFPercentage(3),
+        color: 'white',
+    },
+    rightView: {
+        flex: 1
+    },
+    navi: {
+        width: '100%',
+        height: '10%',
+        backgroundColor: '#F6F6F9',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+
 
     /*
     ===== LOGIN COMPONENT =====
@@ -13,5 +53,25 @@ export default StyleSheet.create({
     /*
     ===== MAIN COMPONENT =====
     */
+
+    infoContainer: {
+        flex:3, 
+        flexDirection:'row',
+        flexWrap:'wrap',
+        justifyContent: 'space-around',
+        alignItems:'center',
+        paddingHorizontal:'5%',
+        backgroundColor: 'pink',
+        
+    },
+
+    infoView: {
+        width: '43%',
+        height:'40%',
+        margin: '3%',
+        padding: '5%',
+        borderRadius:10,
+        backgroundColor:"#ffffff"
+    }
 
 });
