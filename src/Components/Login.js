85756efc7a236a4f089d06cd250bcd3626dd234f
Seonghwan <aiya0422@gmail.com>
2023-01-03 14:30:23 +0900

    IOS용 KeyboardAvoidingView 추가

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -20,42 +20,44 @@ const Login = () => {
     };
 
     return (
-        <View style={styles.loginScreen}>
-            <View style={styles.loginTitle}>
+        <KeyboardAvoidingView style={{ flex: 1 }}>
+            <View style={styles.loginScreen}>
+                <View style={styles.loginTitle}>
+                    <Image
+                        source={require("../../assets/image/logo.png")}
+                        style={styles.TitleIcon}
+                    />
+                    <Text style={styles.TitleText}>XMAS CORE</Text>
+                </View>
+                <View style={styles.loginInput}>
+                    <TextInput
+                        style={styles.InputBox}
+                        placeholder="아이디"
+                        onChangeText={(text) => setId(text)}
+                    />
+                    <TextInput
+                        style={styles.InputBox}
+                        placeholder="패스워드"
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                    <TextInput
+                        style={styles.InputBox}
+                        placeholder="서버 주소"
+                        onChangeText={(text) => setServer(text)}
+                    />
+                    <TouchableOpacity
+                        style={[styles.InputBox, styles.InputButton]}
+                        onPress={OnLogin}
+                    >
+                        <Text style={{ color: "white" }}>로그인</Text>
+                    </TouchableOpacity>
+                </View>
                 <Image
-                    source={require("../../assets/image/logo.png")}
-                    style={styles.TitleIcon}
+                    source={require("../../assets/image/corp.png")}
+                    style={styles.corpName}
                 />
-                <Text style={styles.TitleText}>XMAS CORE</Text>
             </View>
-            <View style={styles.loginInput}>
-                <TextInput
-                    style={styles.InputBox}
-                    placeholder="아이디"
-                    onChangeText={(text) => setId(text)}
-                />
-                <TextInput
-                    style={styles.InputBox}
-                    placeholder="패스워드"
-                    onChangeText={(text) => setPassword(text)}
-                />
-                <TextInput
-                    style={styles.InputBox}
-                    placeholder="서버 주소"
-                    onChangeText={(text) => setServer(text)}
-                />
-                <TouchableOpacity
-                    style={[styles.InputBox, styles.InputButton]}
-                    onPress={OnLogin}
-                >
-                    <Text style={{ color: "white" }}>로그인</Text>
-                </TouchableOpacity>
-            </View>
-            <Image
-                source={require("../../assets/image/corp.png")}
-                style={styles.corpName}
-            />
-        </View>
+        </KeyboardAvoidingView>
     );
 };
 
