da7bf958876961877a5a93be925ccf318dfa70bd
Seonghwan <aiya0422@gmail.com>
2023-02-07 16:20:34 +0900

    키보드와 입력 영역 외의 화면을 터치할 때 키보드 사라지게 수정

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,6 +6,8 @@ import {
     TextInput,
     TouchableOpacity,
     Alert,
+    TouchableWithoutFeedback,
+    Keyboard,
 } from "react-native";
 import React, { useState } from "react";
 import styles from "./Styles";
@@ -38,42 +40,44 @@ const Login = ({ navigation }) => {
             behavior={Platform.OS === "ios" ? "padding" : "height"}
             style={{ flex: 1 }}
         >
-            <View style={styles.loginScreen}>
-                <View style={styles.loginTitle}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.loginScreen}>
+                    <View style={styles.loginTitle}>
+                        <Image
+                            source={require("../../assets/image/logo.png")}
+                            style={styles.TitleIcon}
+                        />
+                        <Text style={styles.TitleText}>XMAS CORE</Text>
+                    </View>
+                    <View style={styles.loginInput}>
+                        <TextInput
+                            style={styles.InputBox}
+                            placeholder="아이디"
+                            onChangeText={(text) => setId(text)}
+                        />
+                        <TextInput
+                            style={styles.InputBox}
+                            placeholder="패스워드"
+                            onChangeText={(text) => setPassword(text)}
+                        />
+                        <TextInput
+                            style={styles.InputBox}
+                            placeholder="서버 주소"
+                            onChangeText={(text) => setServer(text)}
+                        />
+                        <TouchableOpacity
+                            style={[styles.InputBox, styles.InputButton]}
+                            onPress={OnLogin}
+                        >
+                            <Text style={{ color: "white" }}>로그인</Text>
+                        </TouchableOpacity>
+                    </View>
                     <Image
-                        source={require("../../assets/image/logo.png")}
-                        style={styles.TitleIcon}
+                        source={require("../../assets/image/corp.png")}
+                        style={styles.corpName}
                     />
-                    <Text style={styles.TitleText}>XMAS CORE</Text>
                 </View>
-                <View style={styles.loginInput}>
-                    <TextInput
-                        style={styles.InputBox}
-                        placeholder="아이디"
-                        onChangeText={(text) => setId(text)}
-                    />
-                    <TextInput
-                        style={styles.InputBox}
-                        placeholder="패스워드"
-                        onChangeText={(text) => setPassword(text)}
-                    />
-                    <TextInput
-                        style={styles.InputBox}
-                        placeholder="서버 주소"
-                        onChangeText={(text) => setServer(text)}
-                    />
-                    <TouchableOpacity
-                        style={[styles.InputBox, styles.InputButton]}
-                        onPress={OnLogin}
-                    >
-                        <Text style={{ color: "white" }}>로그인</Text>
-                    </TouchableOpacity>
-                </View>
-                <Image
-                    source={require("../../assets/image/corp.png")}
-                    style={styles.corpName}
-                />
-            </View>
+            </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
     );
 };
