9df3138407c5f34553082f2f22d593000bd7a29e
Seonghwan <aiya0422@gmail.com>
2023-04-06 16:30:43 +0900

    update fetch-login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -15,14 +15,14 @@ import * as Keychain from "react-native-keychain";
 const Login = ({ navigation }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [serverAddr, setServerAddr] = useState("");
+    const [serverAddr, setServerAddr] = useState("http://192.168.0.124"); // temporary variable
     const [disable, setDisable] = useState(false);
 
-    // login with states.
-    const tryLogin = async () => {
-        // return username == "1" && password == "1" && serverAddr == "1";
+    const OnLogin = async () => {
+        setDisable(true);
+
         try {
-            const response = await fetch("http://192.168.0.124/api/login", {
+            await fetch(`${serverAddr}/api/login`, {
                 body: JSON.stringify({
                     username: username,
                     password: password,
@@ -32,41 +32,30 @@ const Login = ({ navigation }) => {
                     "Content-Type": "application/json",
                 },
                 method: "POST",
-            });
-            console.log(response);
+            })
+                .then((response) => response.json())
+                .then((resopnseJSON) => console.log(resopnseJSON));
+            // Alert.alert("로그인 성공", "입력하신 정보는 자동으로 저장됩니다.", [
+            //     {
+            //         text: "확인",
+            //     },
+            // ]);
+            // await Keychain.setGenericPassword(
+            //     username,
+            //     password
+            // ).then(async () => {
+            //     // retrieve credentials.
+            //     try {
+            //         // get login info.
+            //         await Keychain.getGenericPassword();
+            //     } catch (error) {
+            //         console.error("Keychain couldn't be accessed!", error);
+            //     }
+            // });
+            // // navigation.navigate("Main");
         } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const OnLogin = async () => {
-        setDisable(true);
-        console.log(
-            ` username: ${username}\n password: ${password}\n serverAddr: ${serverAddr}\n`
-        );
-        if (await tryLogin()) {
-            // login success.
-            Alert.alert("로그인 성공", "입력하신 정보는 자동으로 저장됩니다.", [
-                {
-                    text: "확인",
-                },
-            ]);
-            const userInfo = { username: username, serverAddr: serverAddr };
-            await Keychain.setGenericPassword(
-                JSON.stringify(userInfo),
-                password
-            ).then(async () => {
-                // retrieve credentials.
-                try {
-                    // get login info.
-                    await Keychain.getGenericPassword();
-                } catch (error) {
-                    console.log("Keychain couldn't be accessed!", error);
-                }
-            });
-            // navigation.navigate("Main");
-        } else {
             // login fail.
+            console.error(error);
             Alert.alert(
                 "로그인 실패",
                 "입력하신 서버 주소에 로그인 할 수 없습니다.",
@@ -78,6 +67,7 @@ const Login = ({ navigation }) => {
                 ]
             );
         }
+
         setDisable(false);
     };
 
@@ -85,10 +75,6 @@ const Login = ({ navigation }) => {
     const keyCheck = async () => {
         const credentials = await Keychain.getGenericPassword();
         if (credentials) {
-            const userInfo = JSON.parse(credentials.username);
-            setUsername(userInfo.username);
-            setPassword(credentials.password);
-            setServerAddr(userInfo.serverAddr);
             // navigation.navigate("Main");
         }
     };
