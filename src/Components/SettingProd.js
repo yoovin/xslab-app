cd081431934e03ea917372df63450e7baa0be468
Seonghwan <aiya0422@gmail.com>
2023-05-05 16:38:21 +0900

    세팅 메뉴 추가 및 메뉴별 파일 연결

---
+++ src/Components/SettingProd.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+const SettingProd = () => {
+    return(
+        <View></View>
+    )
+}
+
+export default SettingProd
---
+++ src/Components/SettingTemp.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+const SettingTemp = () => {
+    return(
+        <View></View>
+    )
+}
+
+export default SettingTemp
