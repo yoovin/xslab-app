13d13c53efa3f3b7e35cf8c0e153b8c398018c5a
Seonghwan <aiya0422@gmail.com>
2023-05-16 17:09:03 +0900

    설정메뉴에서 개인정보 삭제, 로그아웃 버튼 추가

--- src/Components/Setting/SettingMenu.js
+++ src/Components/Setting/SettingMenu.js
@@ -1,22 +1,20 @@
 import { TouchableOpacity, View, Text, Image, SafeAreaView, ImageBackground } from 'react-native'
+import { useState } from 'react'
+import Dialog from "react-native-dialog"
 import Icon from 'react-native-vector-icons/Ionicons'
 
 import styles from '../Styles'
+import { RFPercentage } from 'react-native-responsive-fontsize'
 
 const SettingMenu = ({ navigation }) => {
+    const [isLogoutButtonPress, setIsLogoutButtonPress] = useState(false)
+
     return (
         <SafeAreaView style={[styles.screen, styles.screen_setting]}>
             <View style={{ width: '80%', alignItems: 'flex-start' }}>
                 <Text style={[styles.text_title]}>설정</Text>
             </View>
             <View style={[styles.box, styles.box_setting]}>
-                <TouchableOpacity style={[styles.iconList]} onPress={() => {}}>
-                    <Image source={require('../../../assets/image/icon_info.png')} style={[styles.iconList_icon]} />
-                    <View style={[styles.list, styles.iconList_content]}>
-                        <Text style={[styles.text_name]}>개인 정보</Text>
-                        <Icon name='chevron-forward-outline' style={[styles.icon_arrow]} />
-                    </View>
-                </TouchableOpacity>
                 <TouchableOpacity style={[styles.iconList]} onPress={() => navigation.navigate('SettingFan')}>
                     <Image source={require('../../../assets/image/icon_fan.png')} style={[styles.iconList_icon]} />
                     <View style={[styles.list, styles.iconList_content]}>
@@ -39,7 +37,24 @@ const SettingMenu = ({ navigation }) => {
                     </View>
                 </TouchableOpacity>
             </View>
+
+            <TouchableOpacity style={[styles.box, styles.box_setting]} onPress={() => setIsLogoutButtonPress(true)}>
+                <Text style={{ fontSize: RFPercentage(2), color: 'red', paddingVertical: '5%' }}>로그아웃</Text>
+            </TouchableOpacity>
             <ImageBackground source={require('../../../assets/image/vora.png')} style={styles.voraBackground} />
+
+            <Dialog.Container visible={isLogoutButtonPress} contentStyle={styles.dialog}>
+                <Dialog.Description>로그아웃 하시겠습니까?</Dialog.Description>
+                <Dialog.Button
+                    label='예'
+                    color='black'
+                    onPress={() => {
+                        setIsLogoutButtonPress(false)
+                        navigation.reset({ routes: [{ name: 'Logout' }] })
+                    }}
+                ></Dialog.Button>
+                <Dialog.Button label='아니오' color='black' onPress={() => setIsLogoutButtonPress(false)}></Dialog.Button>
+            </Dialog.Container>
         </SafeAreaView>
     )
 }
